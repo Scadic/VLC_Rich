c9075d3b1f53009066aaaf5eff76f82106832fb6
Truls Skadberg <46495721+Scadic@users.noreply.github.com>
2021-02-25 13:40:05 +0100

    Updated vlc_rich.cmd to run pythonw executable not python

--- vlc_rich.cmd
+++ vlc_rich.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 CD "%USERPROFILE%\VLC_Rich"
 CALL .\vlc_rich\Scripts\activate.bat
-vlc_rich.pyw /p <vlc_interface_password>
+pythonw.exe vlc_rich.pyw /p <vlc_interface_password>
 @ECHO ON
